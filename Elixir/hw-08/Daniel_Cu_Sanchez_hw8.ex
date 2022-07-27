86a7b0a3c7bda1a12be84624ed7ff7709eccabc6
Daniel Cu Sánchez <danielcu@alternet.com.mx>
2022-07-27 16:09:51 -0500

    Adding time

--- Elixir/hw-08/Daniel_Cu_Sanchez_hw8.ex
+++ Elixir/hw-08/Daniel_Cu_Sanchez_hw8.ex
@@ -4,8 +4,19 @@
 # init program: iex Daniel_Cu_Sanchez_hw8.ex
 # Hw.Primes.sum_primes(5000)
 # Hw.Primes.sum_primes_parallel(5000)
+# Hw.Primes.time(fn -> Hw.Primes.sum_primes(5000) end)
+# Hw.Primes.time(fn -> Hw.Primes.sum_primes_parallel(5000) end)
+# Hw.Primes.main(500000)
 
 defmodule Hw.Primes do
+
+  # This functions can run both functions sequential and parallel, we can add the number of times
+  def main(times) do
+    Hw.Primes.time(fn -> Hw.Primes.sum_primes(times) end)
+      |> IO.puts()
+    Hw.Primes.time(fn -> Hw.Primes.sum_primes_parallel(times) end)
+      |> IO.puts()
+  end
 
   # sum_primes, that receives a single argument, the top limit of the prime numbers to add. It will return the sum of all prime numbers between 2 and the limit provided. This function will work sequentially.
   def sum_primes(top_limit) do
@@ -16,7 +27,7 @@ defmodule Hw.Primes do
   #A function called sum_primes_parallel, that takes two arguments: the limit of the prime numbers to add, and the number of threads to launch. It returns the sum of all prime numbers between 2 and limit, computed in parallel
   def sum_primes_parallel(until_number) do
     IO.puts("Parallel THREAD")
-    cores = 12 #How many cores we're working with
+    cores = 12 # This number was gotten by System.schedulers
     list_cores = Enum.to_list(1..cores) #Makes a list of [1,..,8]
     block = div(until_number, cores) #Creates a block
     limit_botton = for y <- list_cores do
@@ -39,7 +50,7 @@ defmodule Hw.Primes do
   def sum_primes_range(limit_bottom, limit_top) do
     list_numbers = Enum.to_list(limit_bottom..limit_top-1) #List with limits
     primes_list = for x <- list_numbers do
-      if prime(x) do #Evaluate if its prime
+      if is_prime(x) do #Evaluate if its prime
         x
       else
         0
@@ -49,17 +60,17 @@ defmodule Hw.Primes do
   end
 
   #Auxiliar function patter matching
-  def prime(num)
-    when num == 2 do #Number 2 is the first prime so we're adding a condition here
+  def is_prime(num)
+    when num == 2 do #Number 2 is the first is_prime so we're adding a condition here
       _result = true
   end
 
   #Auxiliar function patter matching
-  def prime(num) do
+  def is_prime(num) do
     limit_top = :math.sqrt(num) #Make the top limit the sqrt(num)
     limit_top = round(limit_top) #Round answer to create a list with bottom limit and top limit
     middle = Enum.to_list(2..limit_top) #Create a list with the limits per number
-    limit_bottom = for x <- middle do #Check if the num is prime or not and make a list
+    limit_bottom = for x <- middle do #Check if the num is is_prime or not and make a list
       if x < 2 do #If num is less than two return false
           false
         else if rem(num, x) == 0 do #Check if the number is divisible by "x" number inside the range
@@ -71,10 +82,17 @@ defmodule Hw.Primes do
     end
 
     if Enum.member?(limit_bottom, false) do #Check if the previous list indicates if "num" could be divided by a numer
-      _result = false #If it has a false then the number isn't prime
+      _result = false #If it has a false then the number isn't is_prime
     else
       _result = true #Else the number is prime
     end
   end
-
+  #This functions mesures the time processing
+  def time(function) do
+    function
+    |> :timer.tc()
+    |> IO.inspect()
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
 end
